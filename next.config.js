a9f2e5321c3b3540ca89937b0d3dab57f1a2c400
Yan <yan@macintosh.lan>
2022-12-04 22:40:38 -0800

    following https://github.com/DaveAldon/Next.js-and-GitHub-Pages-Example

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: './',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
