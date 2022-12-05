14f30af0350c00a5a65a4a152f3845b7ebd5a95f
Yan <yan@macintosh.lan>
2022-12-04 23:34:42 -0800

    moved files over

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
