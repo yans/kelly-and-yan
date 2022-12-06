09bc15b4db7913c075d2812055d78dc676bad2fc
Yan <yan@macintosh.lan>
2022-12-05 22:46:11 -0800

    added meta info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,37 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-
 import React from 'react'
 
+import '../styles/globals.css'
+
+import bg from '../../public/2x/background.jpeg'
+import favicon16 from '../../public/favicon-16x16.png'
+import favicon32 from '../../public/favicon-32x32.png'
+
 export default function App({ Component, pageProps }: AppProps) {
+  const title = "Kelly & Yan"
+  const description = "Join us on our special day."
+  const url = "htpps://kelly.and.yan.co"
   return (
     <>
       <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="robots" content="noindex,nofollow,noarchive" />
+
+        <meta itemProp="name" content={title} />
+        <meta itemProp="description" content={description} />
+        <meta itemProp="image" content={bg.src} />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content={url} />
+        <meta property="og:image" content={bg.src} />
+        <meta property="og:description" content={description} />
+
         <meta name="viewport" content="initial-scale=1, minimum-scale=1" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon32.src} />
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon16.src} />
       </Head>
       <Component {...pageProps} />
     </>
