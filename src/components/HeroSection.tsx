653d666ba27fe1d058d754c1849cdcc9ffff8993
Yan <yan@macintosh.lan>
2022-12-06 00:20:38 -0800

    added scroll prompt

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,6 +12,19 @@ export function HeroSection() {
   const imageContainerRef = React.useRef<HTMLDivElement | null>(null)
   const imageRef = React.useRef<HTMLDivElement | null>(null)
 
+  const hasScrolled = React.useRef<boolean>(false)
+  const [showScrollPrompt, setShowScrollPrompt] = React.useState(false)
+  React.useEffect(() => {
+    const timeout = window.setTimeout(() => {
+      if (!hasScrolled.current) {
+        setShowScrollPrompt(true)
+      }
+    }, 3000)
+    return () => {
+      window.clearTimeout(timeout)
+    }
+  }, [])
+
   const handleScroll = React.useCallback(() => {
     if (!sectionRef.current || !imageContainerRef.current || !imageRef.current) {
       return
@@ -19,6 +32,11 @@ export function HeroSection() {
     const imageContainerOffsetTop = imageContainerRef.current.offsetTop
     const imageContainerHeight = imageContainerRef.current.offsetHeight
     const sectionHeight = sectionRef.current.offsetHeight
+
+    if (imageContainerOffsetTop > 10) {
+      hasScrolled.current = true
+      setShowScrollPrompt(false)
+    }
 
     const progression = imageContainerOffsetTop / (sectionHeight - imageContainerHeight)
 
@@ -83,8 +101,31 @@ export function HeroSection() {
           className={styles.headerContent}
         >
           <Date className={styles.date} month="08" date="18" />
+          {showScrollPrompt && <ScrollPrompt />}
         </div>
       </header>
     </section>
+  )
+}
+
+function ScrollPrompt() {
+  return (
+    <svg
+      className={styles.scrollPrompt}
+      enable-background="new -8.369 -3.009 40.05 35.69"
+      height="35.69px"
+      id="Layer_1"
+      version="1.1"
+      viewBox="-8.369 -3.009 40.05 35.69"
+      width="40.05px"
+    >
+      <g>
+        <path
+          className={styles.scrollPromptPath}
+          d="M12.406,32.394l7.6-7.601l0,0l11.301-11.2c0.5-0.5,0.5-1.199,0-1.699l-2.301-2.301c-0.5-0.5-1.199-0.5-1.699,0l-14.9,14.9
+          c-0.5,0.5-1.2,0.5-1.701,0l-14.8-14.9c-0.5-0.5-1.2-0.5-1.7,0l-2.199,2.2c-0.5,0.5-0.5,1.2,0,1.7l11.199,11.2l0,0l7.601,7.6
+          C11.206,32.793,12.006,32.793,12.406,32.394"/>
+      </g>
+    </svg>
   )
 }
